e07a35728685b0c25df9b0bdfafbfad74e3f95fa
dpw1 <diego.boarutto.fortes@gmail.com>
2019-06-04 00:52:02 +0100

    Updates

---
+++ precache-manifest.bf9261ff12189c621a38307e2ec0d492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c655470e277b4acb2bca6d1f7e51dd08",
+    "url": "/jokes-app/index.html"
+  },
+  {
+    "revision": "52863589a9212a338077",
+    "url": "/jokes-app/static/css/main.ac24086f.chunk.css"
+  },
+  {
+    "revision": "2337e18260d18ecd4674",
+    "url": "/jokes-app/static/js/2.e06ef9c1.chunk.js"
+  },
+  {
+    "revision": "52863589a9212a338077",
+    "url": "/jokes-app/static/js/main.2b225f8c.chunk.js"
+  },
+  {
+    "revision": "535c3fab462afcd181c4",
+    "url": "/jokes-app/static/js/runtime~main.751210b8.js"
+  }
+]);
