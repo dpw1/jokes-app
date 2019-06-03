bff7a0bde3e2149f000f7bb2e513633ac63a2911
dpw1 <diego.boarutto.fortes@gmail.com>
2019-06-04 00:54:47 +0100

    [FIX] typo error handling

--- src/JokeList.js
+++ src/JokeList.js
@@ -37,7 +37,7 @@ export default class JokeList extends Component {
         });
 
         if (!this.seenJokes.has(data.joke)) {
-          jokes.push({ joke: data.joke, id: uuid(), votes: 0 });
+          jokes.push({ joke: data.joke, id: uuid.v4(), votes: 0 });
         } else {
         }
       }
@@ -56,7 +56,8 @@ export default class JokeList extends Component {
       );
     } catch (err) {
       alert("Something went wrong.");
-      this.state.loading({ loading: false });
+      console.log(err);
+      this.setState({ loading: false });
     }
   }
 
@@ -114,7 +115,7 @@ export default class JokeList extends Component {
           </FlipMove>
           {this.state.loading &&
             Array.from({ length: this.props.howManyJokesToLoad }).map(_ => (
-              <Loader key={uuid()} />
+              <Loader key={uuid.v4()} />
             ))}
         </section>
       </div>
