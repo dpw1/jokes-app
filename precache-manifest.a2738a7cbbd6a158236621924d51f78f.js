d83a0245a7456d7aee1fe5db5f41dea8609926b6
dpw1 <diego.boarutto.fortes@gmail.com>
2019-06-04 05:04:36 +0100

    Updates

--- precache-manifest.a2738a7cbbd6a158236621924d51f78f.js
+++ precache-manifest.a2738a7cbbd6a158236621924d51f78f.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0f4ca5adfbb6e27ebce1ce846bbe5c53",
+    "revision": "ab0c6d8943024189f260806092ad8764",
     "url": "/jokes-app/index.html"
   },
   {
-    "revision": "dbb3d30a9abcee3926df",
-    "url": "/jokes-app/static/css/main.ac24086f.chunk.css"
+    "revision": "afe85a10647b0dd377d4",
+    "url": "/jokes-app/static/css/main.2569af68.chunk.css"
   },
   {
     "revision": "2337e18260d18ecd4674",
     "url": "/jokes-app/static/js/2.e06ef9c1.chunk.js"
   },
   {
-    "revision": "dbb3d30a9abcee3926df",
+    "revision": "afe85a10647b0dd377d4",
     "url": "/jokes-app/static/js/main.8d8d742d.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jokes-app/precache-manifest.e289bbb119d159e0984f454723c30747.js"
+  "/jokes-app/precache-manifest.a2738a7cbbd6a158236621924d51f78f.js"
 );
 
 self.addEventListener('message', (event) => {
